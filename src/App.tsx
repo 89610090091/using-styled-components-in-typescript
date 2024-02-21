8a7be33484b7786d5c5138bac0efce113ee2a76f
Aleksandr_k <rzn.russia4912@gmail.com>
2024-02-21 22:47:17 +0300

    .attrs method 	new file:   src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Styled-components <span>.attrs</span> method</Title>
+
+      <Form>
+        <Field placeholder='hello!!!' />
+        <Field />
+        <Field />
+      </Form>
+    </div>
+  );
+}
+
+export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    margin-bottom: 32px;
+    span {
+      color: #11a322;
+    }
+    `;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 500px;
+  `;
+
+const Field = styled.input.attrs(({ placeholder, title }) => ({
+  type: "password",
+  placeholder: placeholder || "Type smth...",
+}))`
+  padding: 5px 15px;
+  margin: 10px 0;
+  width: 100%;
+  font-size: 1rem;
+  `;
